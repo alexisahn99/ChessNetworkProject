1f1dddd8ca646dd97826597d83132bd9ff85b589
Seoyoung Ahn <seoyoungahn@Seoyoungs-MBP.attlocal.net>
2024-03-20 20:05:56 -0500

    controller returns tuples back to the server

--- server/controller/Controller.java
+++ server/controller/Controller.java
@@ -1,10 +1,12 @@
 package server.controller;
 
 import java.util.ArrayList;
-
 import server.model.ChessBoard;
 import server.model.ChessPieces.ChessPiece;
 import server.model.ChessPieces.ChessPieceColor;
+
+// TODO: removing pawn at the end option for now in selectDestination()
+// TODO: when REPAINT we need to send info of what piece is where
 
 public class Controller {
     private ChessBoard board;
@@ -30,87 +32,90 @@ public class Controller {
         }
     }
 
-    public ArrayList<int[]> selectPiece(int fromRow, int fromCol)
-    // always returns movableSquares that a piece can move to. 
-    // It may also return an empty movable squares. 
-    // TODO: what happens when movableSquares.size() == 0
+    public Tuple userPressed(int row, int col)
     {
+        int clickCount = this.board.incrementClickCount();
+
+        if (clickCount == 1)
+        {
+            return this.selectPiece(row, col);
+        }
+        else 
+        {
+            this.board.setClickCount(0); // if invalidMove, you reset selecting a piece. else, validMove
+            return this.selectDestination(row, col);
+        }
+    }
+
+    public Tuple selectPiece(int fromRow, int fromCol)
+    // This function always returns movableSquares that a piece can move to. 
+    // It may also return an empty movable squares. 
+    {
+        boolean isValidMove = true;
         this.currentChessPiece = this.board.getChessPiece(fromRow, fromCol);
         ArrayList<int[]> movableSquares = this.board.getMovableSquares(this.currentChessPiece);
         if (movableSquares.size() == 0)
         {
             this.board.setClickCount(0);
-            ArrayList<int[]> allCurrentPieces = this.board.getPiecesLocation(this.board.getCurrentPlayer());
+            isValidMove = false;
         }
-        return movableSquares;
+        return new Tuple(FunctionFlag.DESTINATION, isValidMove, movableSquares, false, this.getCurrentPlayer());
     }
 
-    public void selectDestination(int toRow, int toCol)
+    public Tuple selectDestination(int toRow, int toCol)
     {
-        this.board.placeChessPiece(toRow, toCol, this.currentChessPiece);
+        FunctionFlag functionFlag = FunctionFlag.SOURCE;
+        boolean isValidMove = false;
+        ArrayList<int[]> allCurrentPieces = null;
+        boolean isGameOver = false;
 
-        // Check if a pawn has reached the opposite end of the board
-        if (this.board.isPawnAtEnd(this.currentChessPiece)) 
-        {
-            // TODO: convert pawn to new piece
+        // 1. Determine whether it is a legal destination
+        ArrayList<int[]> movableSquares = this.board.getMovableSquares(this.currentChessPiece);
+        for (int[] square : movableSquares) {
+            if (square[0] == toRow && square[1] == toCol) {
+                // 1-1. Move is valid, so we return a tuple indicating this
+                isValidMove = true;
+                this.board.placeChessPiece(toRow, toCol, this.currentChessPiece);
+                this.switchPlayers();
+                functionFlag = FunctionFlag.REPAINT;
+            }
         }
-        else 
+        // 1-2. If no valid move was found, we consider the move invalid
+
+        // 2. Check for game over
+        if (this.board.isGameOver()) 
         {
-            this.endOfTurn();
+            // System.out.println("Game over!");
+            isGameOver = true;
         }
+        else
+        {
+            allCurrentPieces = this.board.getPiecesLocation(this.board.getCurrentPlayer());
+        }
+
+        return new Tuple(functionFlag, isValidMove, allCurrentPieces, isGameOver, this.getCurrentPlayer());
     }
 
-    public void userPressed(int row, int col)
-    {
-        int clickCount = this.board.getClickCount();
-        clickCount++;
-        this.board.setClickCount(clickCount);
-
-        if (clickCount == 1)
-        {
-            this.selectPiece(row, col);
-
-        }
-        else if (clickCount == 2)
-        {
-            this.selectDestination(row, col);
-            this.board.setClickCount(0);
-        }
-    }
-
+    // TODO: never used
     public void convertPawn(String unicode, ChessPieceColor color)
     {
         this.board.addNewPiece(this.currentChessPiece.getCurrentRow(), this.currentChessPiece.getCurrentCol(), unicode, color);
         this.endOfTurn();
     }
 
+    // TODO: never used
     public void endOfTurn()
     {
         this.switchPlayers();
-        this.view.updateDisplay();
-
         // Check for game over
         if (this.board.isGameOver()) 
         {
-            WinLossData writeData = new WinLossData();
-            String winner;
-            if (this.getCurrentPlayer() == ChessPieceColor.B)
-            {
-                winner = "White";
-                writeData.addWinLossData("1");
-            }
-            else
-            {
-                winner = "Black";
-                writeData.addWinLossData("0");
-            }
-            this.view.displayWinner(winner);
             System.out.println("Game over!");
         }
         else
         {
             ArrayList<int[]> allCurrentPieces = this.board.getPiecesLocation(this.board.getCurrentPlayer());
-            this.view.enableSquares(allCurrentPieces);
+            // this.view.enableSquares(allCurrentPieces);
         }
     }
 
